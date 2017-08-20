a8ae85c594008507729edaa09694d060b63c2ad8
Thomas Duboucher <thomas@duboucher.eu>
2017-08-20 15:54:13 +0200

    fix order in toc

--- tocify.sh
+++ tocify.sh
@@ -5,7 +5,7 @@ echo "# Table of Contents"
 (for fname in `find * -name "*.md"`; do
     echo \
 "* "\
-`git log --follow --format=%ai $fname | head -n1 | cut -f1 -d" "`\
+`git log --follow --format=%ai $fname | tail -n1 | cut -f1 -d" "`\
 " "\
 "["`<$fname grep -oP "^# \K(.*)"`"]"\
 "(${fname/%.md/.html})"\
